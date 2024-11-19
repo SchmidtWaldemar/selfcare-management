0025ed0f17555abeee1b48d6983159db4b41ee03
Waldemar Schmidt <waldemars.phone@gmail.com>
2024-11-19 10:27:26 +0100

    participant readded in pipeline after hardware upgrade

--- backend/gateway/src/main/java/com/platform/selfcare/config/SecurityConfig.java
+++ backend/gateway/src/main/java/com/platform/selfcare/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.platform.selfcare.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -38,4 +45,22 @@ public class SecurityConfig {
 				);
 		return http.build();
 	}
+
+	/*
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		List<String> allowedOrigins = new ArrayList<String>();
+		allowedOrigins.add("http://192.168.178.157:8222/");
+		allowedOrigins.add("http://192.168.178.157:7080/");
+		allowedOrigins.add("http://localhost:4200/");
+		allowedOrigins.add("http://192.168.178.157:7080/");
+		config.setAllowedOrigins(allowedOrigins);
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("*"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+	*/
 }
