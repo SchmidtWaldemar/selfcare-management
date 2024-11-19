e478edcf024350b6c38bccd55c991cb5e828d007
Waldemar Schmidt <waldemars.phone@gmail.com>
2024-11-19 14:03:11 +0100

    frontend to pipeline

--- frontend/src/app/services/moderator/api-configuration.ts
+++ frontend/src/app/services/moderator/api-configuration.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:8040';
+  rootUrl: string = 'http://192.168.178.157:8040';
 }
 
 /**
