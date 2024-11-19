0d0170ef4af0adf6bf52593f21685cf3690d7ea2
Waldemar Schmidt <waldemars.phone@gmail.com>
2024-11-19 17:53:19 +0100

    dynamic ENV for pipeline

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    backendHost: '${BACKEND_HOST}',
+    backendPort: '${BACKEND_PORT}',
+    backendProtocolSchema: '${BACKEND_PROTOCOL_SCHEMA}',
+    backendModeratorPort:  '${BACKEND_MODERATOR_PORT}'
+};
